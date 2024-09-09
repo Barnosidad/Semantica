fba2710110fcfc7fd13e7ccd0f18906790c57511
Diego Gonzalez <gh098013@gmail.com>
2024-09-09 09:22:21 -0600

    CambioPrueba

--- prueba.cpp
+++ prueba.cpp
@@ -9,10 +9,9 @@ static void Main(String[] args)
     char e, rsi, eax;
     float pi, alpha, theta;
     int rex, ip, sp, a;
-    zared = gei;
     rex = 65535;
+    rex = 70000;
     e = 255;
-    e++;
     e -= 200; // 55
     e += 5; // 60
     e *= 2; // 120
